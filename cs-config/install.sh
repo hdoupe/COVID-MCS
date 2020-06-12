5310a802094145761e74e442d645a696c186cf6a
hdoupe <henrymdoupe@gmail.com>
2020-06-12 13:48:00 -0400

    Trigger build

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,5 +1,6 @@
 # bash commands for installing your package
 # git clone https://github.com/burkeob/COVID-MCS
+echo 2
 git clone -b twodim --depth 1 https://github.com/hdoupe/COVID-MCS
 cd COVID-MCS
 conda install -c conda-forge paramtools pandas rpy2
